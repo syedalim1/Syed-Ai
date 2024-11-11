8570dda9e980fc6247b8ace9d1af5e2e6baa5749
syedalim1 <syedsyed3777@gmail.com>
2024-11-11 10:16:18 +0530

    first commit

---
+++ src/Components/SideBar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Sidebar.css";
+import assit from "../../assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="Sidebar">
+        <img />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
